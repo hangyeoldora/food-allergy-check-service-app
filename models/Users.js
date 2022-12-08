e6d1685577aec3ff4de77b0f96f1cdd7d246210b
hgyeoldora <hg14699@naver.com>
2022-12-08 13:42:18 +0900

    [UPDATE] add allergyList in models.users

--- models/Users.js
+++ models/Users.js
@@ -8,16 +8,17 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      // allergyList: {
+      //   type: DataTypes.STRING,
+
+      // },
     });
   
-    // 연관, 각 유저마다 다양한 포스트
     Users.associate = (models) => {
       Users.hasMany(models.Posts, {
           onDelete: "cascade",
       });
-      // 136
       Users.hasMany(models.Likes, {
-          //해당 게시물과 관련된 모든 댓글 삭제
           onDelete: "cascade"
       });
   }
